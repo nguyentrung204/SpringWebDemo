00476e4b91bce9d141f0bb09a5f7f619598a341e
lntanh <lntanh@vitenet.net>
2018-10-12 14:28:59 +0700

    asdasd

--- src/main/java/controllers/BaseController.java
+++ src/main/java/controllers/BaseController.java
@@ -3,7 +3,7 @@ package controllers;
 import org.springframework.stereotype.Controller;
 
 /**
- * Created by nhs3108 on 6/2/17eee.
+ * Created by nhs3108 on 6/2/17eeexxxxsdfswerwerdf.
  */
 @Controller
 public class BaseController {
